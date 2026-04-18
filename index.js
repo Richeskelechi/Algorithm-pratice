442690ac32722cf4d9c651af125f973993c81b61
Riches <richeskelechi94.rk@gmail.com>
2026-04-18 22:33:53 +0000

    Added the file

---
+++ index.js
@@ -0,0 +1,8 @@
+// Import The Student Data
+import studentData from './data.js';
+// Now you can use studentData as an array of objects
+function classify(allstudent) {
+    // Do Your Solution Inside Here...
+
+}
+console.log(classify(studentData));
